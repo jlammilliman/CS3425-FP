0c819ab62fe2114c637d18f70eda83a91ae6157b
jlmillim <jlammilliman@gmail.com>
2021-12-12 22:37:20 -0500

    instructor evaluation blocks done

--- instructor.php
+++ instructor.php
@@ -2,15 +2,24 @@
     require "instructorDB.php";
     session_start(); 
 
+    if(!isset($_SESSION["ID"])) {
+        header("LOCATION:main.php"); 
+        session_destroy(); 
+    }
+
+
     $courses = GetCourses($_SESSION["ID"]); 
 ?> 
 
 <body>   
-    
+
     <div class="base-div" id="basepain">
         <label margin-left="10px">Courses:</label>
 
         <?php 
+        //=========================================================
+        // List courses block here
+        //=========================================================
             foreach ($courses as $row) { 
                 echo "<div class=\"inner-div\">";
                 echo "<label class=\"Class\">" . $row[0] . " - " . $row[1] ."</label>";
@@ -22,6 +31,9 @@
 
     <label id="studentsList-label" margin-left="10px"></label>
     <?php   
+        //=========================================================
+        // Show Students block here
+        //=========================================================
         foreach($courses as $c) {
             echo "<label display=\"block\"><span id=\"courseID\">Student list for " . $c[0] . " </span></label>";
             echo "<div class=\"inner-div\" id=\"showStuds-div\">";
@@ -34,16 +46,77 @@
         }
     ?> 
     </div>
-    <div class="eval-div" id="bottompain">
+
     <?php   
+        //=========================================================
+        // Evaluation block here
+        //=========================================================
+        $eqs = GetCourseEvalQuestions();
         foreach($courses as $c) {
-            echo "<label display=\"block\"><span id=\"courseID\">Evaluations for " . $c[0] . " </span></label>";
-            echo "<div class=\"inner-div\" id=\"showStuds-div\">";
-            $evals = GetCourseEvals($c[0]);
-            foreach ($evals as $e) { 
-                echo "<label>" . $e[0] . " - " . $e[1] . "</label>";
-                echo "<hr size=\"1\" color=\"gray\" margin=\"1px\">";
+            // show some class analytics -- class response is num of students who have completed the evaluation/totalstudentcount
+            $rRate = GetResponseRate($c[0]);
+            if ($rRate[0][1]==0) {
+                $showCRR = 0.00;
+            } else {
+                $showCRR = round($rRate[0][0]/$rRate[0][1] * 100, 2);
             }
+            echo "<div class=\"eval-div\">";
+            // Start building the class evaluation block
+            echo "<h4 class=\"title-h4\">Evaluations for " . $c[0] . "</h4>";
+            echo "<div class=\"inner2-div\" id=\"showStuds-div\">";
+
+            // show some class responseRate
+            echo "<label display=\"block\">Response Rate: " . $rRate[0][0] . "/" . $rRate[0][1] . " (" . $showCRR . "%)</label>";
+            
+            foreach($eqs as $q) {
+                echo "<div class=\"inner-div\"> <h4 class=\"h4\">" . $q[0] . ") " . $q[1] . "</h4>";
+
+                // if the questiontype is freeresponse-- list the reponses: else multiplechoice-- build the table to display data,
+                //qNum,qPrompt,qType
+                if ($q[2]=="multipleChoice") {
+                    $types = array("Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree");
+
+                    // Build table header
+                    echo "<table class=\"custom-table\">";
+                    echo "<tr>";
+                    echo "<th>Response Option</th>";          
+                    echo "<th>Frequency</th>";
+                    echo "<th>Percent</th>";
+                    echo "<th>Percent Visual</th>";
+                    echo "</tr>";
+                    foreach($types as $t) {
+
+                        // We need to count how many people voted for the specified types
+                        $resps =  GetMultChoiceEvalResp($c[0], $q[0], $t);
+
+                        foreach ($resps as $r) {               
+
+                            // Calculate Frequency
+                            if ($rRate[0][0]==0) {
+                                $showFreq = 0.00;
+                            } else {
+                                $showFreq = round($r[0]/$rRate[0][1] * 100, 2);
+                            }
+
+                            //Build each row-- should be five total, one for each type
+                            echo "<tr>";
+                            echo "<td class=\"td\">" . $t . "</td>";
+                            echo "<td class=\"td\">" . $r[0] . "</td>";
+                            echo "<td class=\"td\">" . $showFreq . "%</td>";
+                            echo "<td class=\"td\"><meter max=\"100\" min=\"0\" value=" . $showFreq . "></meter></td>";
+                            echo "</tr>";  
+                        }
+                    }
+                    echo "</table>"; // End table
+                } else {
+                    $resps = GetFreeRespEval($c[0],$q[0]);
+                    foreach ($resps as $r) {
+                        echo "<br class=\"fr-br\">- " . $r[0] . "</br>";
+                    }
+                }
+                echo "</div>";
+            }
+            echo "</div>";
             echo "</div>";
         }
     ?>     
@@ -68,9 +141,36 @@
         background-color: white;
     }
 
+    .h4 {
+        margin: 0px;
+        padding: 0px;
+        text-align: left;
+        border: 1px solid darkgray;
+        background-color: rgb(230,230,230);
+    }
+
+    .title-h4 {
+        margin: 0px;
+        padding: 0px;
+        text-align: left;
+        border-top: 1px solid darkgray;
+        background-color: lightblue;
+    }
+
+    .fr-br {
+        margin: 0px;
+        padding: 0px;
+        text-align: left;
+        border-top: 1px;
+        border-bottom: 1px;
+        border-style: solid;
+        border-color: lightgray;
+        text-align:top;
+    }
+
     .br {
-        margin: 2px;
-        padding: 2px;
+        margin: 0px;
+        padding: 0px;
         text-align: right;
     }
 
@@ -87,8 +187,20 @@
         padding: 0px;
     }
 
+    .inner2-div {
+        background-color: lightgray;
+        text-align: left;
+        display: block;
+        border: 0px;
+        border-top: 1px;
+        border-bottom:1px;
+        border-color: gray;
+        border-style: solid;
+        margin: 0px;
+        padding: 0px;
+    }
+
     .eval-div {
-        width: 820px;
         margin: 20px;
         display: block;
         padding:4px;
@@ -115,45 +227,5 @@
         border: 1px;
         border-color: gray;
         border-style: solid;
-    }
-    
-    .button{
-        padding: 1ch;
-        border-radius: 4px;
-        color: black;
-        text-align: center;
-        text-decoration: none;
-        display: block;
-        font-size: 14px;
-        transition-duration: 0.3s;
-    }
-
-    .button-inline {
-        border-radius: 4px;
-        float: right;
-        color: black;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 14px;
-        transition-duration: 0.1s;
-    }
-
-    .button:hover {
-        background-color: gray;
-    }
-
-    .button-inline:hover {
-        background-color: gray;
-    }
-
-    .button:active {
-        background-color: darkgray;
-        color: white;
-    }
-
-    .button-inline:active {
-        background-color: darkgray;
-        color: white;
     }
 </style>
--- instructorDB.php
+++ instructorDB.php
@@ -44,33 +44,14 @@
         } 
     }
 
-    function GetCourseEvalQuestions() {
+    function GetResponseRate($cID) {
         //connect to database 
         //retrieve the data and display 
         try { 
             $dbh = connectDB(); 
             
             // Get the list of student names 
-            $statement = $dbh->prepare("SELECT qNum,qType,qPrompt FROM FP_EvalQuestions ORDER BY qNum");
-            $statement->execute(); 
-    
-            return $statement->fetchAll(); 
-            $dbh = null; 
-        } catch (PDOException $e) { 
-            print "Error!" . $e->getMessage() . "<br/>"; 
-            die(); 
-        } 
-    }
-
-    function GetMultChoiceEvalResp($cID, $qNum){
-        //connect to database 
-        //retrieve the data and display 
-        try { 
-            $dbh = connectDB(); 
-            
-            // Get the list of student names 
-            $statement = $dbh->prepare("SELECT qNum,qtype,qPrompt,response,SUM");
-            $statement->bindParam(":qNumID", $qNum); 
+            $statement = $dbh->prepare("SELECT COUNT(DISTINCT(er.sID)) AS studRespone,COUNT(DISTINCT(e.sID)) AS totalClassCount FROM FP_Enroll AS e LEFT JOIN FP_EvalResponses AS er ON e.cID=er.cID WHERE e.cID=:courseID");
             $statement->bindParam(":courseID", $cID); 
             $statement->execute(); 
     
@@ -79,17 +60,56 @@
         } catch (PDOException $e) { 
             print "Error!" . $e->getMessage() . "<br/>"; 
             die(); 
-        } 
+        }  
     }
 
-    function GetFreeRespEval($qNum){
+    function GetCourseEvalQuestions() {
         //connect to database 
         //retrieve the data and display 
         try { 
             $dbh = connectDB(); 
             
             // Get the list of student names 
-            $statement = $dbh->prepare("");
+            $statement = $dbh->prepare("SELECT qNum,qPrompt,qType FROM FP_EvalQuestions");
+            $statement->execute(); 
+    
+            return $statement->fetchAll(); 
+            $dbh = null; 
+        } catch (PDOException $e) { 
+            print "Error!" . $e->getMessage() . "<br/>"; 
+            die(); 
+        } 
+    }
+
+    function GetMultChoiceEvalResp($cID, $qNum, $respType){
+        //connect to database 
+        //retrieve the data and display 
+        try { 
+            $dbh = connectDB(); 
+            
+            // Get the list of student names 
+            $statement = $dbh->prepare("SELECT COUNT(response) FROM FP_EvalResponses WHERE response=:respType AND qNum=:qNumID AND cID=:courseID");
+            $statement->bindParam(":qNumID", $qNum); 
+            $statement->bindParam(":courseID", $cID); 
+            $statement->bindParam(":respType", $respType);
+            $statement->execute(); 
+    
+            return $statement->fetchAll(); 
+            $dbh = null; 
+        } catch (PDOException $e) { 
+            print "Error!" . $e->getMessage() . "<br/>"; 
+            die(); 
+        } 
+    }
+
+    function GetFreeRespEval($cID,$qNum){
+        //connect to database 
+        //retrieve the data and display 
+        try { 
+            $dbh = connectDB(); 
+            
+            // Get the list of student names 
+            $statement = $dbh->prepare("SELECT response FROM FP_EvalResponses WHERE cID=:courseID AND qNum=:qNumID");
             $statement->bindParam(":qNumID", $qNum); 
             $statement->bindParam(":courseID", $cID);  
             $statement->execute(); 
@@ -101,18 +121,6 @@
             die(); 
         } 
     }
-
-    /*SELECT COUNT(DISTINCT(er.sID)) AS studRespone,COUNT(DISTINCT(e.sID)) AS totalClassCount FROM FP_EvalResponses AS er 
-    LEFT JOIN FP_Enroll AS e ON e.cID=er.cID
-    WHERE er.cID="CS1000" // 
-
-    SELECT qNum,qPrompt FROM FP_EvalQuestions//
-
-    SELECT response FROM FP_EvalResponses 
-    WHERE cID="CS1000" AND qNum=2 //
-
-    SELECT COUNT(response) FROM FP_EvalResponses WHERE response="Agree" AND qNum=2//
-    */
 ?>
 
 
